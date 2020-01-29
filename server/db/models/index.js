253aa5af9790bb49667355276c9e9993b9cc4e72
Emma Tilden <e.k.tilden@gmail.com>
2020-01-29 11:38:17 -0600

    userTeamPicModsAssoc

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+// const Team = require('./team')
+// const Picture = require('./picture')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +9,20 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+// User.belongsToMany(Team, {
+//   through: 'UserTeam'
+// });
+// Team.belongsToMany(User, {
+// through: 'UserTeam'
+// });
+
+// Picture.belongsTo(Clue);
+// Clue.hasMany(Picture);
+// Picture.belongsTo(User);
+// User.hasMany(Picture);
+
+
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +30,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User, 
+  Team
 }
---
+++ server/db/models/team.js
@@ -0,0 +1,11 @@
+// const Sequelize = require('sequelize');
+// const db = require('db');
+
+// const Team = db.define('team', {
+//     teamName: {
+//         type: Sequelize.STRING,
+//         unique: true,
+//         allowNull: false,
+//     }
+// })
+// module.exports = Team
