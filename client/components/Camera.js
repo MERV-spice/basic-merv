b950727374593b8575a4ff45401f11db84ba703e
Emma Tilden <e.k.tilden@gmail.com>
2020-01-29 17:04:05 -0600

    camera with buttons but not taking pics

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -1,50 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+// // https://snack.expo.io/@charliecruzan/camerja  <--- Resource for camera info and largely where we sourced our code from
+
+import React , {Component} from 'react'
+import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+import { View, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
-export default function CameraComponent() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+export default class CameraComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hasCameraPermission: null,
+            type: Camera.Constants.Type.back,
+            photo: '',
+            id: 0
+        }
+    }
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+        // console.log('this.camera', this.camera)
+    }
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
+    async snapPhoto() {       
+        // console.log('Button Pressed');
+        if (this.camera) {
+            // console.log('Taking photo');
+            const options = { quality: 1, base64: true, fixOrientation: true, 
+            exif: true};
+            await this.camera.takePictureAsync(options).then(photo => {
+                photo.exif.Orientation = 1;            
+                this.setState({
+                    photo: photo,
+                    id: ++this.state.id
+                });
+            });
+        }
+        let photo = this.state.photo.uri;
+        let id = this.state.id;
+        let plantInfo = await this.identifyPlant(this.state.photo.base64); 
+        this.props.addPlant({id, photo, plantInfo})
+    }
+
+    render() {
+        return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} ref={(ref) => {this.camera = ref}} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: .3,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Ionicons color="white" size={64} name="ios-reverse-camera"/>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={this.snapPhoto.bind(this)}
+                style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    marginLeft: 60
+                  }}
+                >
+                    <MaterialCommunityIcons name="circle-slice-8" size={64} color="white" />
+                </TouchableOpacity>
+            </View>
+          </Camera>
         </View>
-      </Camera>
-    </View>
-  );
+        )
+    }
 }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//     },
+// 	captureButton: {
+		
+// 	}
+// });
