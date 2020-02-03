d9779c569c108c3e2064a6f98234a948e5ec4803
Virge Trotter <vtrotter@u.rochester.edu>
2020-02-03 14:09:59 -0600

    merged w/master

--- server/api/game.js
+++ server/api/game.js
@@ -5,7 +5,7 @@ module.exports = router;
 router.get('/', async (req, res, next) => {
     try {
 	const games = await Game.findAll({
-	    include: [Clue, User]
+	  include: [Clue, User]
 	});
 	res.json(games);
     } catch (err) {
@@ -16,7 +16,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:gameId', async (req, res, next) => {
     try {
 	const game = await Game.findByPk(parseInt(req.params.gameId), {
-	    include: [Clue]
+		include: [Clue]
 	});
 	res.json(game);
     } catch (err) {
