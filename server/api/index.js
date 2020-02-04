383b336570677ed953dabb29e45e5bc131109e78
pozag <49734340+pozag@users.noreply.github.com>
2020-02-04 12:27:32 -0600

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,10 +3,9 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/images', require('./images'));
-router.use('/clues', require('./clues'));
+router.use('/clues', require('./clues')); //possibly not needed
 router.use('/auth', require('../auth'));
 router.use('/games', require('./game'));
-router.use('/clues', require('./clues'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
