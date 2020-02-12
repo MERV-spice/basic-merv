b6d360c02e7d8a5990e5aee0d4fdeac14292edd6
Emma Tilden <e.k.tilden@gmail.com>
2020-02-12 11:48:50 -0600

    delete button fixed and order of render fixed

--- client/ngrok.js
+++ client/ngrok.js
@@ -1,3 +1,5 @@
 const ngrokUrl = '5ddc0d69';
 
-export default `https://${ngrokUrl}.ngrok.io`;
+// export default `https://${ngrokUrl}.ngrok.io`;
+
+export default 'https://agile-earth-32545.herokuapp.com';
